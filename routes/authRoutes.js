ad238c66ddb210a5f2a626e54b1c2fc745d37ba1
Andy <mingchia.andy.liu@gmail.com>
2017-08-19 13:13:14 -0700

    ✨ Add authentication & Add mongo DB
    - Add login & logout
    - Add middleware, cookie-session and passport
    - Add remote mongo DB on mlab.com
    - Add mongoose for ORM
    - Re-strcture the index.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,32 @@
+const passport = require('passport')
+
+module.exports = (app) => {
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    )
+
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google')
+    )
+
+    app.get(
+        '/api/logout',
+        (req, res) => {
+            // attached by passport
+            req.logout()
+            res.send(req.user)
+        }
+    )
+
+    app.get(
+        '/api/current_user',
+        (req, res) => {
+            // req.user is attached by passport
+            res.send(req.user)
+        }
+    )
+}
