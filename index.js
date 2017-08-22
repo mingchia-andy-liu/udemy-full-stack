55ae71d328786bb5dee8345a97da465f68a3ba7a
Andy <mingchia.andy.liu@gmail.com>
2017-08-22 09:56:03 -0700

    🏭 Add Production config
    - Add production for serving files
    - Add postinstall build script

--- index.js
+++ index.js
@@ -28,5 +28,19 @@ app.use(bodyParser.json())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+// serving production assets
+if (process.env.NODE_ENV === 'production') {
+    // express will serve up production assets
+    // eg. main.js or main.css
+    // if express doesn't regonize the route, try to see if the file is in the path
+    app.use(express.static('client/build'))
+
+    // serve index.html iff any other fail to serve anything
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
